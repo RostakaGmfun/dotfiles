6dae8ef0cf7268a2e5b4c8625c988aa4b399ee96
RostakaGmfun <rostawesomegd@gmail.com>
2017-05-22 15:24:25 +0300

    Add org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
     ("ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" default)))
  '(package-selected-packages
    (quote
-    (diff-hl diff-hl-mode vimrc-mode slime nyan-mode key-chord fiplr python-mode magit company racer cmake-mode rust-mode ess nix-mode powerline smooth-scrolling solarized-theme helm evil-leader dracula-theme))))
+    (auto-org-md diff-hl diff-hl-mode vimrc-mode slime nyan-mode key-chord fiplr python-mode magit company racer cmake-mode rust-mode ess nix-mode powerline smooth-scrolling solarized-theme helm evil-leader dracula-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,7 +18,7 @@
 
 (require 'package)
 
-(setq package-list '(diff-hl vimrc-mode slime rainbow-delimiters
+(setq package-list '(org diff-hl vimrc-mode slime rainbow-delimiters
                     key-chord anything cmake-mode
                     company dracula-theme ess
                     evil-leader evil fiplr
@@ -108,3 +108,9 @@
 (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
 (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 (setq inferior-lisp-program "sbcl")
+
+;; Org-mode
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("j" "Diary" entry (file+datetree "~/diary/diary.org")
+         "* %T: %?")))
