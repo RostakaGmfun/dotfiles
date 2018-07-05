51b749244538406272906d0cdcc7f9e93e4644ce
Rostyslav Kurylo <rostawesomegd@gmail.com>
2018-07-05 19:10:21 +0300

    [NixOS] Make configuration.nix modular. Cleanup it up

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,94 +1,159 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  machine = import ./machine.nix;
+  machine-config = {
+    zen = [
+    {
+	imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
+        fileSystems."/" =
+            { device = "/dev/disk/by-uuid/fc60becf-304e-46d3-983c-80ada2068ab9";
+            fsType = "ext4";
+            };
 
+        fileSystems."/data" =
+            {
+            device = "/dev/disk/by-label/data";
+            fsType = "ext4";
+            };
+
+        nix.maxJobs = lib.mkDefault 4;
+
+        # Use the systemd-boot EFI boot loader.
+        boot.loader = {
+            systemd-boot.enable = true;
+            efi.canTouchEfiVariables = true;
+            grub.efiSupport = true;
+            grub.device = "nodev";
+        };
+
+        boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+        boot.kernelModules = [ "kvm-intel" ];
+
+        hardware.nvidiaOptimus.disable = true;
+        hardware.opengl = {
+            enable = true;
+            driSupport = true;
+            driSupport32Bit = true;
+            extraPackages = [ config.boot.kernelPackages.nvidia_x11.out ];
+            extraPackages32 = [ pkgs.pkgsi686Linux.linuxPackages.nvidia_x11.out ];
+        };
+
+        hardware.pulseaudio = {
+            enable = true;
+            support32Bit = true;
+        };
+
+        hardware.bumblebee = {
+            enable = true;
+            driver = "nvidia";
+            connectDisplay = false;
+        };
+
+        services.xserver = {
+            synaptics.enable = true;
+            synaptics.twoFingerScroll = true;
+            synaptics.vertEdgeScroll = true;
+            dpi = 101;
+
+            videoDrivers = [ "intel" ];
+        };
+
+        services.tlp.enable = true;
+
+        services.openvpn.servers.cv = { config = '' config /root/rkurylo/vpn-gw2.ovpn ''; };
+
+        environment.systemPackages = with pkgs; [
+          kicad
+        ];
+        }
+    ];
+  }."${machine.name}";
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  imports = [
+    {
+    nixpkgs.config.allowUnfree = true;
+
+    networking.hostName = machine.name;
+    networking.networkmanager.enable = true;
+
+    virtualisation.virtualbox.host.enable = true;
+    nixpkgs.config.virtualbox.enableExtensionPack = true;
+
+    i18n = {
+        consoleFont = "latarcyrheb-sun32";
+        consoleKeyMap = "us";
+        defaultLocale = "en_US.UTF-8";
+    };
+
+    time.timeZone = "Europe/Kiev";
+
+    environment.systemPackages = with pkgs; [
+        clearlooks-phenix
+	dmenu
+	emacs
+	git
+	godef
+        google-chrome
+        hicolor_icon_theme
+        htop
+	i3lock
+	minicom
+        networkmanagerapplet
+	nox
+	okular
+        oxygen-icons5
+        pavucontrol
+        shared_mime_info
+	slack
+	unzip
+        vim
+	vlc
+        wget
+	xorg.xkill
+	zip
     ];
 
-  nixpkgs.config.allowUnfree = true;
+    environment.variables = { XDG_CURRENT_DESKTOP = "kde"; };
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-    grub.efiSupport = true;
-    grub.device = "nodev";
-  };
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
 
-  hardware.nvidiaOptimus.disable = true;
+    # Enable the X11 windowing system.
+    programs.fish.enable = true;
+    services.xserver = {
+        enable = true;
+        layout = "us,ru,ua";
+        xkbOptions = "grp:alt_shift_toggle, caps:swapescape";
+        displayManager.lightdm.enable = true;
+        displayManager.sessionCommands = ''
+        xrdb -merge ~/.Xresources
+        dbus-launch ${pkgs.networkmanagerapplet}/bin/nm-applet &
+        '';
+        windowManager.i3.enable = true;
+    };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;
 
-  virtualisation.virtualbox.host.enable = true;
+    # The NixOS release to be compatible with for stateful data such as databases.
+    system.stateVersion = "18.03";
 
-  # Select internationalisation properties.
-  i18n = {
-    consoleFont = "latarcyrheb-sun32";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
+    fonts = {
+        fonts = with pkgs; [
+        corefonts
+        inconsolata
+        ];
+    };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Kiev";
+    users.extraGroups.plugdev = { };
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-     wget vim powertop htop gnome3.dconf shared_mime_info networkmanagerapplet
-     virtualbox
-  ];
+    users.extraUsers.gmfun = {
+        extraGroups = ["wheel" "vboxusers" "plugdev"];
+        home = "/home/gmfun";
+        shell = pkgs.fish;
+        isNormalUser = true;
+        initialPassword = "password";
+    };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  services.urxvtd.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us,ru,ua";
-    xkbOptions = "grp:alt_shift_toggle, caps:swapescape";
-    displayManager.lightdm.enable = true;
-    displayManager.sessionCommands = ''
-      xrdb -merge ~/.Xresources
-      dbus-launch ${pkgs.networkmanagerapplet}/bin/nm-applet &
-    '';
-    windowManager.i3.enable = true;
-
-    synaptics.enable = true;
-    synaptics.twoFingerScroll = true;
-    synaptics.vertEdgeScroll = true;
-  };
-
-  services.tlp.enable = true;
-
-  # The NixOS release to be compatible with for stateful data such as databases.
-  system.stateVersion = "17.03";
-
-  fonts = {
-    fontconfig.dpi = 156;
-    fontconfig.ultimate.enable = true;
-    fonts = with pkgs; [
-      corefonts
-      inconsolata
-    ];
-  };
-
-  programs.fish = {
-    enable = true;
-  };
-
-  programs.light.enable = true;
-
-  users.extraUsers.gmfun = {
-    extraGroups = ["wheel" "vboxusers"];
-    home = "/home/gmfun";
-    shell = pkgs.fish;
-    initialPassword = "password";
-  };
-
+    }
+  ] ++ machine-config;
 }
