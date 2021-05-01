699baaf2b9a5c2ab89f0c42ca50366a274805db6
Rostyslav Kurylo <rostawesomegd@gmail.com>
2021-05-01 15:39:40 +0300

    [Emacs] Complete rewrite for Emacs 26

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,154 +1,131 @@
 (require 'package)
-
-(setq package-list '(plantuml-mode company-nixos-options
-                     evil-magit git-gutter ox-twbs org diff-hl
-                     vimrc-mode slime rainbow-delimiters key-chord
-                     anything cmake-mode company dracula-theme ess
-                     evil-leader evil fiplr goto-chg grizzl helm
-                     helm-core julia-mode magit git-commit
-                     magit-popup nix-mode popup powerline
-                     python-mode racer f rust-mode s
-                     smooth-scrolling solarized-theme undo-tree
-                     with-editor dash async go-mode flymake-go
-                     rtags helm-rtags company-rtags flycheck-rtags))
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'prog-mode-hook (lambda ()
-                            (font-lock-add-keywords nil
-                             '(("\\<\\(FIXME\\|TODO\\|BUG\\)" 1 font-lock-warning-face t)))))
-
+;;
+;; Appearance settings
+;;
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-(setq shell-file-name "/bin/sh")
-
-;; evil
-
-(require 'evil)
-(evil-mode 1)
-
-(define-key evil-normal-state-map (kbd ";") 'evil-ex)
-
-(require 'key-chord)
-(setq key-chord-two-keys-delay 0.1)
-(key-chord-mode 1)
-(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-(key-chord-define evil-insert-state-map "qw" 'evil-write)
-(key-chord-define evil-normal-state-map "qw" 'evil-write)
-
-(add-to-list 'default-frame-alist '(font . "DeJaVu Sans Mono-12"))
-
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq-default c-basic-offset 4)
-(c-set-offset 'arglist-cont-nonempty '++)
-(c-set-offset 'innamespace 0)
-(c-set-offset 'inextern-lang 0)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-to-list 'default-frame-alist '(font . "DeJaVu Sans Mono-11"))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
-(global-linum-mode 1)
-(setq-default truncate-lines t)
+(global-display-line-numbers-mode t)
+;;(setq display-line-numbers "%4d \u2502 ")
+(pixel-scroll-mode 1)
+(setq scroll-margin 30)
+(setq scroll-conservatively 101)
+
 (setq-default show-trailing-whitespace t)
-(setq system-time-locale "C")
-(setq split-width-threshold nil)
 
-(require 'smooth-scrolling)
-(smooth-scrolling-mode 1)
+;;
+;; Other Emacs settings
+;;
 
-(load-theme 'dracula t)
-
-(require 'powerline)
-(powerline-default-theme)
-
-(setq make-backup-files nil)
 (setq auto-save-default nil)
-
+;; Disable exit hotkey
 (global-unset-key (kbd "C-x C-c"))
 
-(global-git-gutter-mode 1)
-(setq-default git-gutter:modified-sign "~")
-(setq-default git-gutter:added-sign "+")
-(setq-default git-gutter:deleted-sign "\u2013") ;; en dash
+;; Disable customize-
+(setq-default custom-file null-device)
 
-(set-face-foreground 'git-gutter:modified "orange")
-(set-face-foreground 'git-gutter:added "green")
-(set-face-foreground 'git-gutter:deleted "red")
+;;
+;; Formatting/indentation
+;;
+(setq-default truncate-lines t)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default c-basic-offset 4)
+(c-set-offset 'arglist-cont-nonempty '++)
+(c-set-offset 'innamespace [0])
+(c-set-offset 'inextern-lang [0])
 
-;; Helm config
+;;
+;; Packages
+;;
 
-(require 'helm-config)
-(helm-mode 1)
+(eval-when-compile
+  (require 'use-package)
+  (require 'use-package-ensure)
+  (setq use-package-always-ensure t))
 
-(global-set-key (kbd "M-x") 'helm-M-x)
+(use-package rainbow-delimiters
+    :hook (prog-mode . rainbow-delimiters-mode))
 
-;; Company config
+(use-package evil
+    :init
+    (setq evil-want-keybinding nil)
+    :config
+    (evil-mode 1))
 
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
-(global-company-mode)
-(push 'company-rtags company-backends)
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-SPC") #'company-complete)))
+(use-package evil-collection
+    :init
+    (evil-collection-init))
 
-;; fiplr
+(use-package magit
+    :bind ("C-c m" . 'magit-status)
+    :hook (magit-mode-hook . evil-magit-init))
 
-(require 'fiplr)
-(global-set-key (kbd "C-x f") 'fiplr-find-file)
-(global-set-key (kbd "C-c f") 'fiplr-find-file-other-window)
-(setq-default fiplr-ignored-globs '((directories (".git" "build")) (files ("#*" "*~"))))
+(use-package key-chord
+    :config
+    (key-chord-mode 1)
+    (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+    (key-chord-define evil-insert-state-map "qw" 'evil-write)
+    (key-chord-define evil-normal-state-map "qw" 'evil-write))
 
-;; Racer config
+;; Powerful status line
+(use-package powerline
+    :config
+    (powerline-center-evil-theme))
 
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
+;; Highlight unstaged changes in files
+(use-package git-gutter
+    :config
+    (global-git-gutter-mode 1)
+    (setq-default git-gutter:modified-sign "~")
+    (setq-default git-gutter:added-sign "+")
+    (setq-default git-gutter:deleted-sign "\u2013") ;; en dash
+    (set-face-foreground 'git-gutter:modified "orange")
+    (set-face-foreground 'git-gutter:added "green")
+    (set-face-foreground 'git-gutter:deleted "red"))
 
-;; SLIME
-(require 'slime)
-(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-(setq inferior-lisp-program "sbcl")
+(use-package ivy
+    :config
+    (ivy-mode 1)
+    (setq-default ivy-extra-directories nil)
+    (setq-default ivy-initial-inputs-alist nil)
+    :bind ("C-x p" . 'ivy-switch-buffer))
 
-;; Org-mode
-(global-set-key (kbd "C-c c") 'org-capture)
-(setq org-capture-templates
-      '(("j" "Diary" entry (file "~/diary/diary.org")
-         "** %^{prompt|%T} %?" :prepend :unnarrowed (:empty-lines 1))))
-(setq org-todo-keywords '((sequence "TODO" "DOING" "|" "DONE")))
-(setq org-todo-keyword-faces '(("TODO" . org-warning)
-                               ("DOING" . (:foreground "green" :weight bold))
-                               ("DONE" . "cyan")))
-;; magit
-(defun magit-split ()
+(use-package smex
+    :config
+    (smex-initialize))
+
+;; counsel-fzf but search from the root of current git repository if possible
+(defun my/counsel-fzf-from-git-root ()
+    (interactive)
+    (let ((initial-dir (or (counsel--git-root) default-directory)))
+      (counsel-fzf nil initial-dir nil)))
+
+;; ag (grep) with symbol at point as initial search text
+(defun my/counsel-ag-at-point ()
   (interactive)
-  (if (< (length (window-list)) 2)
-      (split-window-horizontally)())
-  (magit-status))
-(global-set-key (kbd "C-c m") 'magit-status)
-(add-hook 'magit-mode-hook 'evil-magit-init)
+  (counsel-ag (ivy-thing-at-point)))
 
-;; plantuml-mode
-;; Use system `plantuml` executable from default Nix profile rather
-;; than  plain JAR file
-(setq plantuml-jar-path "")
-(setq plantuml-java-command "plantuml")
-(setq plantuml-java-args "")
+(use-package counsel
+    :bind (("C-x f" . 'my/counsel-fzf-from-git-root)
+           ("M-x" . 'counsel-M-x)
+           ("C-x d" . 'counsel-dired)
+           ("C-x g" . 'my/counsel-ag-at-point)))
 
-(setq rtags-socket-file "/tmp/rdm.socket")
-(setq rtags-autostart-diagnostics t)
-(setq rtags-completions-enabled t)
-(setq rtags-display-result-backend 'helm)
-(add-hook 'prog-mode-hook (lambda () (global-set-key (kbd "C-c C-j") 'rtags-find-symbol-at-point)))
+(use-package dracula-theme)
+
+(use-package org
+    :config
+    (setq org-todo-keywords '((sequence "TODO" "DOING" "|" "DONE")))
+    (setq org-todo-keyword-faces '(("TODO" . org-warning)
+                                   ("DOING" . (:foreground "green" :weight bold))
+                                   ("DONE" . "cyan"))))
+
+(use-package cmake-mode)
+(use-package markdown-mode)
